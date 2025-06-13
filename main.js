2b5de23665a6ee757494783a1f08e108fedfbe86
Leta <marysue19951995@gmail.com>
2025-06-13 08:53:42 +0300

    Не работает блок условий(частично)

--- main.js
+++ main.js
@@ -1,44 +1,18 @@
-/**
- * 
- * @param {number} num 
- */
-let isEven = function (num) {
-  const input = prompt(`Введите число, ${name} `) ?? '-';
-  const num = +input;
-  return num === random_number;
+let greet = function () {
+  return 'Привет! Меня зовут,' + this.name;
 };
-while (true) {
-  switch (num === random_number)
-
-    if (num === random_number) {
-    console.log("Вы выиграли!!");
-    break;
-  }
-  if (input === "stop") {
-    console.log("Игра  остановлена");
-    break;
-  }
-  if (num > random_number) {
-    console.log("Вы ввели число больше! Попробуйте снова!");
-    continue;
-  }
-  if (num < random_number) {
-    console.log("Вы ввели число меньше! Попробуйте снова!");
-    continue;
-  }
-  if (num === input) {
-    console.log("Ошибка: вы ввели не число.");
-    continue;
-  }
-  if (num > 20) {
-    console.log("Введите число от 0 до 20");
-    continue;
-  }
-  if (num < 0) {
-    console.log("Введите число от 0 до 20");
-    continue;
-  }
-}
 
 
+let person = {
+  name: 'Leta',
+  age: 29,
+  greet: greet
+};
 
+let anotherPerson = {
+  name: 'Рома',
+  age: '31',
+  greet: greet
+};
+console.log(person.greet());
+console.log(anotherPerson.greet());
